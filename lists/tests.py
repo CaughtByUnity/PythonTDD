f57a74509cc4048e8c355668de4d39aad0e1c98b
caughtbyunity <xammer3500@gmail.com>
2025-09-28 22:38:23 +0300

    Рефакторизовано представление домашней страницы для использования шаблона

--- lists/tests.py
+++ lists/tests.py
@@ -1,25 +1,13 @@
 from django.test import TestCase
-from django.urls import resolve
-from django.http import HttpRequest
 
 from lists.views import home_page
-from urllib3 import request
-
 
 # Create your tests here.
 class HomePageTest(TestCase):
-    '''тест домашней страницы'''
-    def test_root_url_resolves_to_home_page_view(self):
-        '''тест: корневой url преобразуется в представление домашней страницы'''
-        found = resolve('/')
-        self.assertEqual(found.func, home_page)
-
-    def test_home_page_returns_correct_html(self):
+    def test_uses_home_template(self):
         '''тест: домашняя страница возвращает корректный html'''
-        # создаём объект HttpRequest
-        request = HttpRequest()
-        # передаём его представлению home_page, которое даёт отклик
-        response = home_page(request)
+        # передаём в self.client.get адрес, который хотим протестировать
+        response = self.client.get('/')
         # извлекаем содержимое .content отклика, конвертируя их в html кодировки utf8 при помощи .decode
         html = response.content.decode('utf8')
         # Проверка, что полученная html-строка начинается с тэга <html>
@@ -28,3 +16,4 @@ class HomePageTest(TestCase):
         self.assertIn('<title>To-Do lists</title>', html)
         # Проверка, что полученная html-строка заканчивается тэгом </html>
         self.assertTrue(html.endswith('</html>'))
+        self.assertTemplateUsed(response, 'home.html')
