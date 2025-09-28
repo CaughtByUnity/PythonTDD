047d08df656182fa585d98c59fdc25a6ad4fd368
caughtbyunity <xammer3500@gmail.com>
2025-09-28 19:18:59 +0300

    ФТ теперь проверяет возможность ввести элемент списка

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,10 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
+
+from selenium.webdriver.chrome.service import Service
 
 class NewVisitorTest(unittest.TestCase):
     '''тест нового посетителя'''
@@ -21,18 +25,30 @@ class NewVisitorTest(unittest.TestCase):
         # Она видит, что заголовок и шапка страницы говорят о списках
         # неотложных дел
         self.assertIn('To-Do', self.browser.title)
-        self.fail('закончить тест!')
+        header_text = self.browser.find_element(By.CSS_SELECTOR, 'h1').text
+        self.assertIn('To-Do', header_text)
         # Ей сразу же предлагается ввести элемент списка
-
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
         # Она набирает в текстовом поле "Купить павлиньи перья" (её хобби -
         # вязание рыболовных мушек
+        inputbox.send_keys('Купить павлиньи перья')
         # Когда она нажимает энтер, страница обновляется, и теперь страница
         # содержит: "1: Купить павлиньи перья" в качестве элемента списка
-
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+        table = self.browser.find_element(By.ID, 'id_list_table')
+        rows = table.find_elements(By.CSS_SELECTOR, 'tr')
+        self.assertTrue(
+            any(row.text == '1. Купить павлиньи перья' for row in rows)
+        )
         # Текстовое поле по-прежнему приглашает её добавить ещё один элемент.
         # Она вводит "Сделать мушку из павлиньих перьев"
         # (Эдит очень методична)
-
+        self.fail('Закончить тест!')
         # Страница снова обновляется, и теперь показывает оба элемента её списка
 
         # Эдит интересно, запомнит ли сайт её список. Далее она видит, что
