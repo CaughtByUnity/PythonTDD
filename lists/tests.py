a356b50a7ee6152d3129429ab899aa2b548a52ff
caughtbyunity <xammer3500@gmail.com>
2025-09-28 18:52:10 +0300

    Базовое представление теперь возвращает минимальный html

--- lists/tests.py
+++ lists/tests.py
@@ -1,6 +1,10 @@
 from django.test import TestCase
 from django.urls import resolve
+from django.http import HttpRequest
+
 from lists.views import home_page
+from urllib3 import request
+
 
 # Create your tests here.
 class HomePageTest(TestCase):
@@ -9,3 +13,18 @@ class HomePageTest(TestCase):
         '''тест: корневой url преобразуется в представление домашней страницы'''
         found = resolve('/')
         self.assertEqual(found.func, home_page)
+
+    def test_home_page_returns_correct_html(self):
+        '''тест: домашняя страница возвращает корректный html'''
+        # создаём объект HttpRequest
+        request = HttpRequest()
+        # передаём его представлению home_page, которое даёт отклик
+        response = home_page(request)
+        # извлекаем содержимое .content отклика, конвертируя их в html кодировки utf8 при помощи .decode
+        html = response.content.decode('utf8')
+        # Проверка, что полученная html-строка начинается с тэга <html>
+        self.assertTrue(html.startswith('<html>'))
+        # Проверяем правильность содержимого <title>
+        self.assertIn('<title>To-Do lists</title>', html)
+        # Проверка, что полученная html-строка заканчивается тэгом </html>
+        self.assertTrue(html.endswith('</html>'))
