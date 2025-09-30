d64a57ee15c4aeb3b83aafd536092cb832eb8bea
caughtbyunity <xammer3500@gmail.com>
2025-09-30 23:57:03 +0300

    migrate on VSCode

--- lists/tests.py
+++ lists/tests.py
@@ -11,12 +11,36 @@ class HomePageTest(TestCase):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
 
-    def test_can_saave_a_POST_request(self):
+    def test_only_saves_items_when_necessary(self):
+        '''тест: сохраняет элементы, только когда это нужно'''
+        self.client.get('/')
+        self.assertEqual(Item.objects.count(), 0)
+
+    # Код с душком: тест пост-запроса слишком длинный?
+    def test_can_save_a_POST_request(self):
         '''тест: можно сохранить post-запрос'''
         # Вызываем self.client.post для выполнения post-запроса
+        self.client.post('/', data={'item_text': 'A new list item'})
+        # Один новый объект Item был сохранён в базе данных .objects.count()
+        self.assertEqual(Item.objects.count(), 1)
+        # objects.first() - то же самое, что и objects.all()[0]
+        new_item = Item.objects.first()
+        # текст элемента правильный
+        self.assertEqual(new_item.text, 'A new list item')
+
+    def test_redirects_after_POST(self):
+        '''тест: переадресует после POST-запроса'''
         response = self.client.post('/', data={'item_text': 'A new list item'})
-        self.assertIn('A new list item', response.content.decode())
-        self.assertTemplateUsed(response, 'home.html')
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['location'], '/')
+
+    def test_displays_all_list_items(self):
+        '''тест: отображаются все элементы списка'''
+        Item.objects.create(text='itemey 1')
+        Item.objects.create(text='itemey 2')
+        response = self.client.get('/')
+        self.assertIn('itemey 1', response.content.decode())
+        self.assertIn('itemey 2', response.content.decode())
 
 class ItemModelTest(TestCase):
     '''тест модели элемента списка'''
