c443daa0fd7dbc1d9df6a581aa4802ad053867bf
caughtbyunity <xammer3500@gmail.com>
2025-10-01 23:11:21 +0300

    Перевод functional_tests в приложения, использован LiveServerTestCase для уборки ФТ за собой

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,3 +1,4 @@
+from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
@@ -6,7 +7,7 @@ import unittest
 
 from selenium.webdriver.chrome.service import Service
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
     '''тест нового посетителя'''
     def setUp(self):
         '''установка'''
@@ -27,7 +28,7 @@ class NewVisitorTest(unittest.TestCase):
         '''тест: можно начать список и получить его позже'''
         # Эдит слышала про крутое новое онлайн приложение со списком
         # неотложных дел. Она решает оценить его домашнюю страницу
-        self.browser.get('http://localhost:8000')
+        self.browser.get(self.live_server_url)
         # Она видит, что заголовок и шапка страницы говорят о списках
         # неотложных дел
         self.assertIn('To-Do', self.browser.title)
